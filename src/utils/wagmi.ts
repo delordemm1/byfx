07db2f31de771770574a4b447559b611883653f6
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2025-01-30 04:33:50 +0100

    Update wagmi.ts

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,22 +1,22 @@
 
 // import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { WagmiAdapter, } from '@reown/appkit-adapter-wagmi'
-import { bsc } from '@reown/appkit/networks'
+import { bsc, mainnet, arbitrum } from '@reown/appkit/networks'
 import {
   createAppKit,
 } from '@reown/appkit/react'
-// import { cookieStorage, createStorage } from 'wagmi';
+import { cookieStorage, createStorage } from 'wagmi';
 export const projectId = import.meta.env.VITE_PROJECT_ID || 'b56e18d47c72ab683b10814fe9495694';
 // export const projectId = '296924cdb9a40ac2bfe6b78e60779e09';
 // export const projectId = '6ab2057d9cb439a116d445df1477ab2a';
-export const networks = [bsc];
+export const networks = [bsc, mainnet, arbitrum];
 // import { bsc } from 'wagmi/chains';
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
-  // storage: createStorage({
-  //   storage: cookieStorage
-  // }),
+  storage: createStorage({
+    storage: cookieStorage
+  }),
   networks,
   projectId,
   ssr: true,
@@ -27,7 +27,7 @@ createAppKit({
   debug: true,
   adapters: [wagmiAdapter],
   projectId,
-  networks: [bsc],
+  networks: [bsc, mainnet, arbitrum],
   enableWalletGuide: true,
   allWallets: "SHOW",
   metadata: {
@@ -38,10 +38,13 @@ createAppKit({
     url: import.meta.env.VITE_DOMAIN_URL || 'https://byforex.coinconnect.tech',
     icons: ['https://avatars.githubusercontent.com/u/37784886']
   },
+  defaultNetwork: bsc,
   features: {
     // analytics: true,
-    connectMethodsOrder: ['wallet'],
-    walletFeaturesOrder: [],
+    socials: false,
+    email: false,
+    // connectMethodsOrder: [''],
+    walletFeaturesOrder: ['receive', 'send'],
   },
   themeMode: "dark"
 })
